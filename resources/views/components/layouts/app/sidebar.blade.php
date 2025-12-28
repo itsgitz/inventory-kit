5098c25e0504c259ea579d94485c8c800252506b
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-12-28 16:25:29 +0700

    feat(category-management): Add category management feature
    - Introduce Category\Manager Livewire component for category management.
    - Add a new web route '/categories' to access the category manager.
    - Update the application sidebar navigation with a link to the category manager.

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -13,7 +13,20 @@
 
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')" class="grid">
-                    <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+                    <flux:navlist.item
+                        icon="home"
+                        :href="route('dashboard')"
+                        :current="request()->routeIs('dashboard')"
+                        wire:navigate>
+                        {{ __('Dashboard') }}
+                    </flux:navlist.item>
+                    <flux:navlist.item
+                        icon="tag"
+                        :href="route('categories.manager')"
+                        :current="request()->routeIs('categories.manager')"
+                        wire:navigate>
+                        {{ __('Categories') }}
+                    </flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 

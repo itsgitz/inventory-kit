ca6991128e63b4933746fb014296bf9350379b7b
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-12-28 16:28:36 +0700

    chore: updated page title for /categories

--- app/Livewire/Category/Manager.php
+++ app/Livewire/Category/Manager.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\Category;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Manager extends Component
 {
+    #[Title('Categories')]
     public function render()
     {
         return view('livewire.category.manager');
